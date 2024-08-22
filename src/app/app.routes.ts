030f9a0204cecd585bb9bc981fef216c51a76ddd
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-08-22 10:27:15 -0300

    Feat: Guard e Rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoggedComponent } from './pages/logged/logged.component';
+import { AuthGuard } from './guard/auth-guard.guard';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'login',
         pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: LoggedComponent,
+        canActivate: [AuthGuard],
+        children: [
+
+        ]
     },
     {
         path: 'login',
--- src/app/guard/auth-guard.guard.ts
+++ src/app/guard/auth-guard.guard.ts
@@ -1,5 +1,26 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { catchError, map, Observable, of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-export const authGuardGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      // this.router.navigate(['feed']);
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+
+}
