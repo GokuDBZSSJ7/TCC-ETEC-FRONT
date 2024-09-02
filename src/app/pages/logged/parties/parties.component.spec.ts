09f0e638f9df65c31991ba93cd876bfad4426469
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-09-02 09:45:40 -0300

    Testes Unitários

--- src/app/pages/logged/parties/parties.component.spec.ts
+++ src/app/pages/logged/parties/parties.component.spec.ts
@@ -1,23 +1,112 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PartiesComponent } from './parties.component';
+import { AuthService } from '../../../services/auth.service';
+import { StateService } from '../../../services/state.service';
+import { CityService } from '../../../services/city.service';
+import { PartyService } from '../../../services/party.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('PartiesComponent', () => {
   let component: PartiesComponent;
   let fixture: ComponentFixture<PartiesComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let stateService: jasmine.SpyObj<StateService>;
+  let cityService: jasmine.SpyObj<CityService>;
+  let partyService: jasmine.SpyObj<PartyService>;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getUser']);
+    const stateServiceSpy = jasmine.createSpyObj('StateService', ['all']);
+    const cityServiceSpy = jasmine.createSpyObj('CityService', ['all']);
+    const partyServiceSpy = jasmine.createSpyObj('PartyService', ['all']);
+
     await TestBed.configureTestingModule({
-      imports: [PartiesComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        HttpClientTestingModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        NgSelectModule,
+        CommonModule,
+        FormsModule,
+        RouterModule,
+        MatTableModule,
+        MatPaginatorModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+        PartiesComponent // Importando o componente standalone
+      ],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: StateService, useValue: stateServiceSpy },
+        { provide: CityService, useValue: cityServiceSpy },
+        { provide: PartyService, useValue: partyServiceSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PartiesComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    stateService = TestBed.inject(StateService) as jasmine.SpyObj<StateService>;
+    cityService = TestBed.inject(CityService) as jasmine.SpyObj<CityService>;
+    partyService = TestBed.inject(PartyService) as jasmine.SpyObj<PartyService>;
+
+    authService.getUser.and.returnValue({ name: 'Test User' });
+    stateService.all.and.returnValue(of([]));
+    cityService.all.and.returnValue(of([]));
+    partyService.all.and.returnValue(of([]));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize with states and parties', () => {
+    expect(stateService.all).toHaveBeenCalled();
+    expect(partyService.all).toHaveBeenCalled();
+    expect(component.states).toEqual([]);
+    expect(component.dataSource.data).toEqual([]);
+  });
+
+  it('should update cities when state is changed', () => {
+    const citiesMock = [{ id: 1, name: 'City1' }];
+    cityService.all.and.returnValue(of(citiesMock));
+
+    // Ajustando para usar o tipo correto
+    component.selectedStateId = { id: 1 } as any;
+    component.onStateChange();
+
+    expect(cityService.all).toHaveBeenCalledWith({ id: 1 } as any);
+    expect(component.cities).toEqual(citiesMock);
+  });
+
+  it('should clear cities if no state is selected', () => {
+    component.selectedStateId = null;
+    component.onStateChange();
+    expect(component.cities).toEqual([]);
+  });
+
+  it('should get the user from AuthService', () => {
+    expect(authService.getUser).toHaveBeenCalled();
+    expect(component.user).toEqual({ name: 'Test User' });
+  });
+
+  it('should create form on init', () => {
+    expect(component.form).toBeDefined();
   });
 });
--- src/app/pages/logged/politicians/politicians.component.spec.ts
+++ src/app/pages/logged/politicians/politicians.component.spec.ts
@@ -1,23 +1,99 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PoliticiansComponent } from './politicians.component';
+import { UserService } from '../../../services/user.service';
+import { StateService } from '../../../services/state.service';
+import { CityService } from '../../../services/city.service';
+import { PartyService } from '../../../services/party.service';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('PoliticiansComponent', () => {
   let component: PoliticiansComponent;
   let fixture: ComponentFixture<PoliticiansComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+  let stateService: jasmine.SpyObj<StateService>;
+  let cityService: jasmine.SpyObj<CityService>;
+  let partyService: jasmine.SpyObj<PartyService>;
 
   beforeEach(async () => {
+    const userServiceSpy = jasmine.createSpyObj('UserService', ['getPoliticians', 'filterUser']);
+    const stateServiceSpy = jasmine.createSpyObj('StateService', ['all']);
+    const cityServiceSpy = jasmine.createSpyObj('CityService', ['all']);
+    const partyServiceSpy = jasmine.createSpyObj('PartyService', ['all']);
+
     await TestBed.configureTestingModule({
-      imports: [PoliticiansComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        PoliticiansComponent // Importando o componente standalone
+      ],
+      providers: [
+        FormBuilder,
+        { provide: UserService, useValue: userServiceSpy },
+        { provide: StateService, useValue: stateServiceSpy },
+        { provide: CityService, useValue: cityServiceSpy },
+        { provide: PartyService, useValue: partyServiceSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PoliticiansComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    stateService = TestBed.inject(StateService) as jasmine.SpyObj<StateService>;
+    cityService = TestBed.inject(CityService) as jasmine.SpyObj<CityService>;
+    partyService = TestBed.inject(PartyService) as jasmine.SpyObj<PartyService>;
+
+    userService.getPoliticians.and.returnValue(of([]));
+    stateService.all.and.returnValue(of([]));
+    cityService.all.and.returnValue(of([]));
+    partyService.all.and.returnValue(of([]));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize with users, states, and parties', () => {
+    expect(userService.getPoliticians).toHaveBeenCalled();
+    expect(stateService.all).toHaveBeenCalled();
+    expect(partyService.all).toHaveBeenCalled();
+    expect(component.users).toEqual([]);
+    expect(component.states).toEqual([]);
+    expect(component.parties).toEqual([]);
+  });
+
+  it('should create a form with default values', () => {
+    expect(component.form).toBeTruthy();
+    expect(component.form.value).toEqual({
+      name: null,
+      city_id: null,
+      state_id: null,
+      party_id: null,
+    });
+  });
+
+  it('should update cities when state is changed', () => {
+    const citiesMock = [{ id: 1, name: 'City1' }];
+    cityService.all.and.returnValue(of(citiesMock));
+
+    component.selectedStateId = 1;
+    component.onStateChange();
+
+    expect(cityService.all).toHaveBeenCalledWith(1);
+    expect(component.cities).toEqual(citiesMock);
+  });
+
+  // it('should apply filter and update users', () => {
+  //   const usersMock = [{ id: 1, name: 'User1' }];
+  //   userService.filterUser.and.returnValue(of(usersMock));
+
+  //   component.applyFilter();
+
+  //   expect(userService.filterUser).toHaveBeenCalledWith(component.form.value);
+  //   expect(component.users).toEqual(usersMock);
+  // });
 });
